72d8b4f1f578ff5394b0d9c30e293b5fe46754c0
Andre Machado Eurochem <andre.machado@eurochemsam.com>
2024-10-30 11:38:43 -0300

    primeiro commit exercicios 1 e 2

---
+++ main.py
@@ -0,0 +1,12 @@
+# Exercício 1 - Digita o Nome e Retorna quantidade de Carácteres
+
+#print(len(input('Digite o seu nome: ')))
+
+# Exercício 2 - Usuário digita dois valores e programa faz a soma
+
+valor1 = float(input('Informe o primeiro valor: '))
+valor2 = float(input('Informe o segundo valor: '))
+soma = valor1 + valor2
+print(soma)
+
+# Exercício 3 - 
