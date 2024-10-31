7e20ea85f018099b6ebf99cd9daf6231e673597a
Andre Machado Eurochem <andre.machado@eurochemsam.com>
2024-10-31 08:11:22 -0300

    ajuste de sintaxe dos exercicios

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+'''
 # Exercício 1 - Digita o Nome e Retorna quantidade de Carácteres
 
-#print(len(input('Digite o seu nome: ')))
+nome=input('Digite o seu nome: ')
+quantidade_caracteres = len(nome)
+print(quantidade_caracteres)
 
+'''
 # Exercício 2 - Usuário digita dois valores e programa faz a soma
 
-valor1 = float(input('Informe o primeiro valor: '))
-valor2 = float(input('Informe o segundo valor: '))
+valor1 = int(input('Informe o primeiro valor: '))
+valor2 = int(input('Informe o segundo valor: '))
 soma = valor1 + valor2
 print(soma)
 
